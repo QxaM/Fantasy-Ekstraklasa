6606620737be9120068594d228f422bbc9e44b23
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-05-28 16:59:04 +0200

    ApiFootball - added paging recurence function

--- src/main/java/com/kodilla/fantasy/apifootball/facade/ApiFootballFacade.java
+++ src/main/java/com/kodilla/fantasy/apifootball/facade/ApiFootballFacade.java
@@ -1,11 +1,13 @@
 package com.kodilla.fantasy.apifootball.facade;
 
 import com.kodilla.fantasy.apifootball.client.ApiFootballClient;
+import com.kodilla.fantasy.apifootball.dto.GetPlayersDto;
 import com.kodilla.fantasy.apifootball.dto.GetTeamsDto;
 import com.kodilla.fantasy.apifootball.dto.TeamResponseDto;
 import com.kodilla.fantasy.domain.Team;
 import com.kodilla.fantasy.mapper.ApiFootballMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ApiFootballFacade {
 
     private final ApiFootballClient apiFootballClient;
@@ -25,5 +28,21 @@ public class ApiFootballFacade {
                 .map(TeamResponseDto::getTeam)
                 .map(apiFootballMapper::mapToTeam)
                 .collect(Collectors.toList());
+    }
+
+    private List<GetPlayersDto> playerPaging(int paging, List<GetPlayersDto> playersList) {
+        GetPlayersDto apiFootballResponse = apiFootballClient.fetchPlayers(paging);
+        playersList.add(apiFootballResponse);
+
+        if(apiFootballResponse.getPaging().getCurrent() < apiFootballResponse.getPaging().getTotal()) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                log.error("Waiting interrupted " + e.getMessage());
+            }
+            paging++;
+            playerPaging(paging, playersList);
+        }
+        return playersList;
     }
 }
