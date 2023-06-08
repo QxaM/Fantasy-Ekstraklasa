372cbc91cde9a54d87aee86bc6ea56d113d0e16c
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-06-08 23:44:47 +0200

    User - added entity, repository and service

---
+++ src/main/java/com/kodilla/fantasy/domain/User.java
@@ -0,0 +1,23 @@
+package com.kodilla.fantasy.domain;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "USERNAME")
+    private String username;
+}
---
+++ src/main/java/com/kodilla/fantasy/service/UserDbService.java
@@ -0,0 +1,33 @@
+package com.kodilla.fantasy.service;
+
+import com.kodilla.fantasy.domain.User;
+import com.kodilla.fantasy.domain.exception.ElementNotFoundException;
+import com.kodilla.fantasy.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDbService {
+
+    private final UserRepository repository;
+
+    public List<User> getUsers() {
+        return repository.findAll();
+    }
+
+    public User getUser(Long id) throws ElementNotFoundException {
+        return repository.findById(id)
+                .orElseThrow(() -> new ElementNotFoundException("User with given id: " + id + "does not exist"));
+    }
+
+    public User saveUser(User user) {
+        return repository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        repository.deleteById(id);
+    }
+}
