afe9fd5d696d0e82dfea70d0ec00b72547e8613a
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-06-08 23:12:19 +0200

    League - added enitity, repository and dbservice

---
+++ src/main/java/com/kodilla/fantasy/domain/League.java
@@ -0,0 +1,23 @@
+package com.kodilla.fantasy.domain;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "LEAGUES")
+public class League {
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "LEAGUE_NAME")
+    private String name;
+}
---
+++ src/main/java/com/kodilla/fantasy/service/LeagueDbService.java
@@ -0,0 +1,33 @@
+package com.kodilla.fantasy.service;
+
+import com.kodilla.fantasy.domain.League;
+import com.kodilla.fantasy.domain.exception.ElementNotFoundException;
+import com.kodilla.fantasy.repository.LeagueRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LeagueDbService {
+
+    private final LeagueRepository repository;
+
+    public List<League> getLeagues() {
+        return repository.findAll();
+    }
+
+    public League getLeague(Long id) throws ElementNotFoundException {
+        return repository.findById(id)
+                .orElseThrow(() -> new ElementNotFoundException("League with given id: " + id + "does not exist"));
+    }
+
+    public League createLeague(League league) {
+        return repository.save(league);
+    }
+
+    public void deleteLeague(Long id) {
+        repository.deleteById(id);
+    }
+}
