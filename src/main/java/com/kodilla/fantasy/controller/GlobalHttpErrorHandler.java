6242ca9c57e7b1d75b148607ce90308e74774819
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-06-10 16:54:01 +0200

    GlobalHttpErrorHandler - updated

--- src/main/java/com/kodilla/fantasy/controller/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/fantasy/controller/GlobalHttpErrorHandler.java
@@ -1,6 +1,8 @@
 package com.kodilla.fantasy.controller;
 
 import com.kodilla.fantasy.domain.exception.ElementNotFoundException;
+import com.kodilla.fantasy.domain.exception.NotEnoughFundsException;
+import com.kodilla.fantasy.domain.exception.SquadAlreadyFullException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,5 +15,15 @@ public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ElementNotFoundException.class)
     public ResponseEntity<Object> handleElementNotFoundException(ElementNotFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SquadAlreadyFullException.class)
+    public ResponseEntity<Object> handleSquadAlreadyFullException(SquadAlreadyFullException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(NotEnoughFundsException.class)
+    public ResponseEntity<Object> handleNotEnoughFundsException(NotEnoughFundsException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_ACCEPTABLE);
     }
 }
