5fa2392fbb0d1bbde548b0476b55db480a9ddaeb
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-06-11 21:59:30 +0200

    Added LivScore facade tests

--- src/test/java/com/kodilla/fantasy/livescore/facade/LiveScoreFacadeTests.java
+++ src/test/java/com/kodilla/fantasy/livescore/facade/LiveScoreFacadeTests.java
@@ -4,6 +4,7 @@ import com.kodilla.fantasy.domain.Player;
 import com.kodilla.fantasy.domain.Position;
 import com.kodilla.fantasy.domain.Team;
 import com.kodilla.fantasy.livescore.client.LiveScoreClient;
+import com.kodilla.fantasy.livescore.domain.Event;
 import com.kodilla.fantasy.livescore.domain.Match;
 import com.kodilla.fantasy.livescore.domain.dto.*;
 import com.kodilla.fantasy.livescore.domain.exception.CouldNotMapElement;
@@ -57,6 +58,29 @@ public class LiveScoreFacadeTests {
     }
 
     @Test
+    void testAddEvents() {
+        //Given
+        GetEventsDto getEventsDto = new GetEventsDto(new ArrayList<>());
+        EventDataDto eventDto1 = new EventDataDto("YELLOW_CARD", "Firstname Lastname", 1, new ArrayList<>());
+        EventDto eventDto2 = new EventDto("GOAL", "Firstname Lastname", 1);
+        EventDto eventDto3 = new EventDto("GOAL_ASSIST", "Firstname 1 Lastname 1", 2);
+        EventDataDto eventDto4 = new EventDataDto(null, null, 0, List.of(eventDto2, eventDto3));
+        getEventsDto.getEvents().addAll(List.of(eventDto1, eventDto4));
+
+        Team team1 = new Team(1L, 1L, "Test", "TET", new ArrayList<>());
+        Team team2 = new Team(2L,2L, "Team 2", "TE2", new ArrayList<>());
+        Match emptyMatch = new Match("1", team1, team2, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+
+        when(liveScoreClient.fetchEvents("1")).thenReturn(getEventsDto);
+
+        //When
+        facade.addEvents(emptyMatch);
+
+        //Then
+        verify(liveScoreMapper, times(1)).mapEvents(emptyMatch, getEventsDto);
+    }
+
+    @Test
     void testFindMatches() throws CouldNotMapElement {
         //Given
         LiveScoreTeamDto teamDto1 = new LiveScoreTeamDto("Test team 1");
@@ -94,15 +118,26 @@ public class LiveScoreFacadeTests {
         Team team2 = new Team(2L,2L, "Team 2", "TE2", new ArrayList<>());
         Player player1 = new Player(3L, 3L, "Firstname", "Lastname", 21, BigDecimal.ZERO, Position.GK, team1, new ArrayList<>());
         Player player2 = new Player(3L, 3L, "Firstname 1", "Lastname 1", 21, BigDecimal.ZERO, Position.GK, team2, new ArrayList<>());
-        Match match = new Match("1", team1, team2, List.of(player1), List.of(player2), new ArrayList<>());
+        Event event1 = new Event("YELLOW_CARD", player1);
+        Event event2 = new Event("GOAL", player2);
+        Event event3 = new Event("GOAL_ASSIST", player1);
+        Match match = new Match("1", team1, team2, List.of(player1), List.of(player2), List.of(event1, event2, event3));
 
         LiveScoreTeamDto teamDto1 = new LiveScoreTeamDto("Test team 1");
         LiveScoreTeamDto teamDto2 = new LiveScoreTeamDto("Team 2");
         MatchDto matchDto = new MatchDto("1", teamDto1, teamDto2);
         GetMatchesDto getMatchesDto = new GetMatchesDto(List.of(matchDto, matchDto));
 
+        GetEventsDto getEventsDto = new GetEventsDto(new ArrayList<>());
+        EventDataDto eventDto1 = new EventDataDto("YELLOW_CARD", "Firstname Lastname", 1, new ArrayList<>());
+        EventDto eventDto2 = new EventDto("GOAL", "Firstname Lastname", 1);
+        EventDto eventDto3 = new EventDto("GOAL_ASSIST", "Firstname 1 Lastname 1", 2);
+        EventDataDto eventDto4 = new EventDataDto(null, null, 0, List.of(eventDto2, eventDto3));
+        getEventsDto.getEvents().addAll(List.of(eventDto1, eventDto4));
+
         when(liveScoreClient.fetchLineups("1")).thenReturn(getLineupsDto);
         when(liveScoreClient.fetchMatches(1)).thenReturn(getMatchesDto);
+        when(liveScoreClient.fetchEvents("1")).thenReturn(getEventsDto);
         when(liveScoreMapper.mapToMatchList(getMatchesDto)).thenReturn(List.of(match));
 
         //When
@@ -111,6 +146,7 @@ public class LiveScoreFacadeTests {
         //Then
         assertAll(() -> assertEquals(1, fetchedMatches.size()),
                 () -> assertEquals("Firstname", fetchedMatches.get(0).getLineup1().get(0).getFirstname()),
-                () -> assertEquals("Firstname 1", fetchedMatches.get(0).getLineup2().get(0).getFirstname()));
+                () -> assertEquals("Firstname 1", fetchedMatches.get(0).getLineup2().get(0).getFirstname()),
+                () -> assertEquals(3, fetchedMatches.get(0).getEvents().size()));
     }
 }
